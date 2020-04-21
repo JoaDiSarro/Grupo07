2c57fdf3d883e7235800ccf612ca1501f8311e33
LucianoFrangolini <64044827+LucianoFrangolini@users.noreply.github.com>
2020-04-21 13:14:52 -0300

    Getters y setters

--- Client/src/Modelo/ClasePrueba.java
+++ Client/src/Modelo/ClasePrueba.java
@@ -11,4 +11,30 @@ public class ClasePrueba
 	{
         
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getHola() {
+		return hola;
+	}
+
+	public void setHola(String hola) {
+		this.hola = hola;
+	}
+	
+	
 }
