22962bcfd4a9a488c22803fca0f4eb282402c5b9
LucianoFrangolini <64044827+LucianoFrangolini@users.noreply.github.com>
2020-04-26 13:43:56 -0300

    Clase entrenador lista

--- Client/src/modeloClases/Carta.java
+++ Client/src/modeloClases/Carta.java
@@ -4,12 +4,19 @@ package modeloClases;
  * @author Frangolini,Luciano.<br>
  * Clase que representa una carta utilizada por participantes de un torneo pokémon.
  */
-public abstract class Carta implements ICarta {
+public abstract class Carta implements ICarta,Cloneable{
 
 	/**
 	 *Método encargado de hechizar un objeto de tipo IHechizable.<br>
 	 *@param hechizable, de tipo IHechizable : Objeto a hechizar.<br>
 	 */
 	public abstract void hechizar(IHechizable hechizable);
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+	
+	
 
 }
